55e2da2b42d5b917c9ba5a2b5770227eaa8d83fc
Aniruddh Singh <singhaniruddh30@gmail.com>
2021-09-12 01:19:16 +0530

    web uid resolved

--- app/src/main/java/com/Hackthon/botshop/MainActivity.java
+++ app/src/main/java/com/Hackthon/botshop/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
    private SignInButton btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+//hello
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
