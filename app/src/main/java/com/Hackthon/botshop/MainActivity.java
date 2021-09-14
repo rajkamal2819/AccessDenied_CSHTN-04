fb1ea3f420d15267b3502bf1027169f86caf81d4
Aniruddh Singh <singhaniruddh30@gmail.com>
2021-09-14 13:51:32 +0530

    bitmap issue resolved

--- app/src/main/java/com/Hackthon/botshop/MainActivity.java
+++ app/src/main/java/com/Hackthon/botshop/MainActivity.java
@@ -158,8 +158,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Log.i(LOG_TAG,"ResultCode: "+resultCode);
