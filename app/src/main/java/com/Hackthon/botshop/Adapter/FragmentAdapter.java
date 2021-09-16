fd2a34250e191599bafa8b864abda4ddc0a74614
Rajkamal <rajkamal.singh1011@gmail.com>
2021-09-16 20:34:30 +0530

    commit: Packages for Fragments and ViewPager , TabLayout are created for chat rooms and also small visual changes are done

---
+++ app/src/main/java/com/Hackthon/botshop/Adapter/FragmentAdapter.java
@@ -0,0 +1,51 @@
+package com.Hackthon.botshop.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.Hackthon.botshop.Fragmentss.CallsFragment;
+import com.Hackthon.botshop.Fragmentss.ChatsFragment;
+
+public class FragmentAdapter extends FragmentPagerAdapter  {
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    public FragmentAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0: return new ChatsFragment();
+            case 1: return new CallsFragment();
+            default: return new ChatsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        String title = null;
+        if(position==0){
+            title = "CHATS";
+        }
+        else if(position==1){
+            title = "CALLS";
+        }
+
+        return title;
+    }
+}
--- app/src/main/java/com/Hackthon/botshop/FashionDomain.java
+++ app/src/main/java/com/Hackthon/botshop/FashionDomain.java
@@ -1,19 +1,30 @@
 package com.Hackthon.botshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.Hackthon.botshop.Adapter.FragmentAdapter;
+import com.google.android.material.tabs.TabLayout;
+
 public class FashionDomain extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fashion_domain);
 
-        //just checking by pull req if visible
-        //now just checking weather i am a collaborator
-         //kjjsajfosojfjsdkjf
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+
+
 
     }
 }
